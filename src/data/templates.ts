96134ebec624e90c242195e78a2421f4433e8702
Akshay <akshay.allen26200@gmail.com>
2025-01-02 19:49:37 +0530

    Image fix 2

--- src/data/templates.ts
+++ src/data/templates.ts
@@ -1,44 +1,52 @@
+import template1Background from '../assets/empty_page_1.jpg';
+import template2Background from '../assets/empty_page_2.jpg';
+import template3Background from '../assets/empty_page_3.jpg';
+import template4Background from '../assets/empty_page_4.jpg';
+import template5Background from '../assets/empty_page_5.jpg';
+import template6Background from '../assets/empty_page_6.jpg';
+
+
 export const templates = [
   {
     id: 'template1',
     name: 'Your Template 1',
-    background: '/assets/empty_page_1.jpg',
-    font: 'Arial', 
-    preview: '/assets/empty_page_1.jpg'
+    background: template1Background,
+    font: 'Arial',
+    preview: template1Background,
   },
   {
     id: 'template2',
     name: 'Your Template 2',
-    background: '/src/assets/empty_page_2.jpg',
+    background: template2Background,
     font: 'Inter',
-    preview: '/src/assets/empty_page_2.jpg'
+    preview: template2Background,
   },
   {
     id: 'template3',
     name: 'Your Template 3',
-    background: '/src/assets/empty_page_3.jpg',
+    background: template3Background,
     font: 'Roboto',
-    preview: '/src/assets/empty_page_3.jpg'
+    preview: template3Background,
   },
   {
     id: 'template4',
     name: 'Your Template 4',
-    background: '/src/assets/empty_page_4.jpg',
+    background: template4Background,
     font: 'Helvetica',
-    preview: '/src/assets/empty_page_4.jpg'
+    preview: template4Background,
   },
   {
     id: 'template5',
     name: 'Your Template 5',
-    background: '/src/assets/empty_page_5.jpg',
+    background: template5Background,
     font: 'Georgia',
-    preview: '/src/assets/empty_page_5.jpg'
+    preview: template5Background,
   },
   {
     id: 'template6',
     name: 'Your Template 6',
-    background: '/src/assets/empty_page_6.jpg',
+    background: template6Background,
     font: 'Times New Roman',
-    preview: '/src/assets/empty_page_6.jpg'
+    preview: template6Background,
   },
 ];
